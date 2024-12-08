e9eb1b0c149bffc68eb3345cdf019ea44eadddab
Thelta04 <mrcomedy014@gmail.com>
2024-12-08 02:02:15 +0000

    shiii

--- pword.py
+++ pword.py
@@ -5,12 +5,15 @@
 
 import re
 import sys, os, time
-from multiprocessing import Process, Value, Queue, Lock, Semaphore, Array
+from multiprocessing import Process, Value, Queue, Lock, Array
 import signal 
+import datetime
 
 counter = Value("i")
 counter.value = 0
+processedLines = Value("i", 0)
 interrupted = Value("b", False)
+lock = Lock() #initializes the lock that will be used throughtout the program
 
 #Open file and convert it to a list of all lines.
 
@@ -24,30 +27,59 @@ def filesToArray(*files):
     Ensures:
     A list with all the lines of the file without punctuation or symbols.
     '''
-    
+    fileNumLines = []
     linesArray = []
     for file in files:  
         with open(file, "r", encoding="utf-8") as f:
             linesArray += f.readlines()
-        
-    return linesArray
+            fileNumLines.append(len(f.readlines()))
 
+        
+    return linesArray, fileNumLines
+
+
+def parcial_results(interval, start_time, mode, array, numLines, numFiles, log_file=None):
+    """
+    """
+    global processedLines
+
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime("%d/%m/%Y-%H:%M:%S")
+    elapsed_time = (time.time() - start_time) * 1000000
+
+    count_atm = 0
+
+    if mode == "c":
+        count_atm = counter.value
+
+    else:
+        for counter in array:
+            count_atm += counter
+    
+    lock.acquire()
+
+    totalLines = 0
+    processedFiles = 0
+
+    for lines in numLines:
+        totalLines += lines
+        if processedLines.value >= totalLines:
+            processedFiles += 1
+
+    return str(timestamp) + str(elapsed_time) + str(count_atm) + str(processedFiles) + str(numFiles - processedFiles)
 
 #Signal stop
 
 def signal_handler(sig, frame):
     """
-    Manipula o sinal SIGINT (Ctrl+C). Ajusta a flag global para interromper o processamento
-    de novos blocos e permite que os processos filhos concluam suas tarefas atuais.
+    
     """
     global interrupted
-    with interrupted.get_lock():
-        interrupted.value = True
     print("\nInterrupção recebida (Ctrl+C). Finalizando processos...")
+    interrupted = True 
 
 
-
-def count_total(lines, search):
+def count_total(lines, search, counter, lock):
     """
     Count all occurrences of the word in the text, even if the word is repeated.
 
@@ -58,57 +90,66 @@ def count_total(lines, search):
     Ensures:
     Print an int of all the occurrences of the word in the file.
     """
-
-    global counter
-
-    #Start the timer
-    start_time = time.time()
+    global processedLines
 
     for line in lines:
         if interrupted.value:
             break
         line = line.strip().lower()
-        with counter.get_lock():
-            counter.value += line.count(search.lower())
+  
 
-    elapsed_time = time.time() - start_time
+        lock.aquire()#Enters the critical section 
 
-    # print("Process " + str(os.getpid()) + ": Counted " + str(counter) + ", took " + str(round(elapsed_time, 1)) + " seconds.")
-
+        counter.value += line.count(search.lower())
+        processedLines.value += 1
+    
+        lock.release()
+            
 
 #Line Counter
 
-def count_lines(lines, search, q, counter, index, lock):
+def count_lines(lines, search, queue, index_counter, lock, counter_array):
     '''
-    Count how many distinct lines contain the word.
+    Searches every line that has the word that needs to be searched
+    and adds them into a set, 
 
-    Requires: lines is a list of lines from the .txt file to be searched.
-    search is the word to search for in the lines.
-    q is a multiprocessing.Queue for storing sets of lines found by each process.
-    counter is a multiprocessing.Array for partial counts from each process.
-    index is the index in the array to store the current process's result.
-    lock is a multiprocessing.Lock to synchronize updates to shared data.
+    Requires:
+    lines is a list with the lines in the .txt that will be searched;
+    search is a string form of the word to search for.
 
     Ensures:
-    Adds the count of lines containing the word to the shared array and queue.
+    A set() with all the lines that have the word in inside of it.
     '''
+    global processedLines
     results = set()
-
+    
     for line in lines:
         line = line.strip().lower()
         if search in line:
             results.add(line)
 
-    # Add the set to the queue
-    q.put(results)
+            
+        lock.acquire()#opens the critical section
 
-    with lock:
-        counter[index] = len(results)
+        #puts the sets into the queue
+        queue.put(results)
+        #adds all the sets of each son into a specific inside of the array
+        counter_array[index_counter] = len(results)
+        processedLines.value += 1
 
+        lock.release()#closes it
+    
+    lock.acquire()#opens the critical section
+        
+    #puts the sets into the queue
+    queue.put(results)
 
+    lock.release()#closes it
+    print(sum(counter_array))
+    
 #counts the number of times the word appears in isolation
 
-def count_isolated(lines, search):
+def count_isolated(lines, search, queue, index_counter, lock, counter_array):
     '''
     Count the times the word appears in the file, bt only when it appears isolated
     (separated by spaces, punctuation, etc.), excluding cases where the word is part of another word.
@@ -121,9 +162,7 @@ def count_isolated(lines, search):
     Print an int of the number of times the search word appears in the file, also says the pid of the process
     and the time it took to get the result.
     '''
-
-    #Start the timer
-    start_time = time.time()
+    global processedLines
 
     counter = 0
     #Create a word pattern to seach using regex. escape() remove special characters
@@ -139,11 +178,30 @@ def count_isolated(lines, search):
         #Count occurrences of the word using regex
         counter += len(re.findall(pattern, line))
 
-    elapsed_time = time.time() - start_time
+        #Enters the critical section --------
+        lock.acquire()
 
+        counter_array[index_counter] = counter
+        processedLines.value += 1
+        
+        lock.release()
+        #exits the lock --------
 
-    print("Process " + str(os.getpid()) + ": Counted " + str(counter) + ", took " + str(round(elapsed_time, 1)) + " seconds.")
+    #Enters the critical section --------
+    lock.acquire()
 
+    queue.put(counter)#Adds to the created queue how many times the word was found inside of this block of text
+    
+    lock.release()
+    #exits the lock --------
+
+    
+def gather_queue(queue, lock):
+    """
+    This function gather all the values that were put on a queue, as a FIFO method, and 
+    makes it that the father adds them all.  ISTO FAZ REFERENCIA A 1 PONTO DO 2.1 DPS DISCUTIR 
+    SE FAZ SENTIDO FAZER UMA FUNCAO FORA DAS OUTRAS OU NAO <<<<<<<<< 
+    """
 
 #Selects the operation and runs the process
 
@@ -156,7 +214,6 @@ def main(args):
     print('Programa: pword.py\n')
 
     global counter
-
     # Register the signal handler for SIGINT (Ctrl+C)
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -164,7 +221,9 @@ def main(args):
     operation = args[0]
     n_process = int(args[1])
     search_word = args[2]
-    files = args[3]
+    interval = int(args[3])
+    log_file = args[4]
+    files = args[5]
 
     #Global Variables
     lines_list = filesToArray(files)
@@ -173,12 +232,16 @@ def main(args):
     
     #Define what function to execute
     if operation == "i":
+        q = Queue()
+        counter_array = Array("i", n_process)
         func = count_isolated
 
     elif operation == "c":
         func = count_total
 
     elif operation == "l":
+        q = Queue()
+        counter_array = Array("i", n_process)
         func = count_lines
 
 
@@ -196,20 +259,18 @@ def main(args):
         chunk = lines_list[start : start + chunk_size]
 
         start += chunk_size
-
-        processes.append(Process(target=func,args=(chunk, search_word)))
-    
+        if (func == count_isolated or func == count_lines):
+            processes.append(Process(target=func,args=(chunk, search_word, q, i, lock, counter_array)))
+        else:
+            processes.append(Process(target=func,args=(chunk, search_word, n_process)))  
+              
     for process in processes:
         process.start()
 
-    try:
-       
-        for process in processes:
-            process.join()
-    except KeyboardInterrupt:
-        print("\nProcessamento interrompido pelo usuário.")
-
-    print(f"Resultados acumulados até o momento: {counter.value}")
+    for process in processes:
+        process.join()
+    
+    print("CONTED:", str(counter.value))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
